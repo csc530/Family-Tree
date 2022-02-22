a2b0eed761e0b6eba6c02609afcfb9f9ab4a5efd
csc530 <cscousin@lakeheadu.ca>
2022-02-22 14:44:53 -0500

    feat(home-screen): Activities
    Added text to redirect to the authentication screen

--- app/src/main/java/com/csc530/familytree/LaunchActivity.kt
+++ app/src/main/java/com/csc530/familytree/LaunchActivity.kt
@@ -1,16 +1,30 @@
 package com.csc530.familytree
 
-import android.app.Activity
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
 import com.csc530.familytree.databinding.ActivityLaunchBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
 
 class LaunchActivity : AppCompatActivity() {
+	private lateinit var auth: FirebaseAuth
 	private lateinit var binding: ActivityLaunchBinding
+	
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_launch)
+		binding = ActivityLaunchBinding.inflate(layoutInflater)
+		setContentView(binding.root)
+		
+		// ? Remove the login and sign up text with a log out button if they are signed in
+		auth = Firebase.auth
+		if(auth.currentUser != null) {
+			//TODO: remove login and sign up options
+			//Todo add logout button
+			binding.vllAuth.removeAllViews()
+		}
+		binding.vllAuth.removeAllViewsInLayout()
 		
 		binding = ActivityLaunchBinding.inflate(layoutInflater)
 		setContentView(binding.root)
@@ -23,5 +37,26 @@ class LaunchActivity : AppCompatActivity() {
 			val intent = Intent(this, LoadTreeActivity::class.java)
 			startActivity(intent)
 		}
+		//switch intents to authentication if they selected login or signup
+		binding.vllAuth.setOnClickListener {
+			val intent = Intent(this, AuthActivity::class.java)
+			startActivity(intent)
+		}
+		
 	}
+	
+	public override fun onStart() {
+		super.onStart()
+		// Check if user is signed in (non-null) and update UI accordingly.
+		val currentUser = auth.currentUser
+		if(currentUser != null) {
+			//TODO: add reload method that will get rid of the log in or sign up prompt/controls
+			reload()
+		}
+	}
+	
+	/**
+	 * Reloads the page to not display the sign in options but a logout button.
+	 */
+	fun reload() {}
 }
