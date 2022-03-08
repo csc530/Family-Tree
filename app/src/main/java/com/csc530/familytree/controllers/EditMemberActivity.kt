3a789b323b29fe0d43db4771a7620972a144ddc5
csc530 <cscousin@lakeheadu.ca>
2022-03-08 15:53:06 -0500

    :tada: create(views): Activities
    Created activity to create and edit a family member
    
    Signed-off-by: csc530 <cscousin@lakeheadu.ca>

---
+++ app/src/main/java/com/csc530/familytree/controllers/EditMemberActivity.kt
@@ -0,0 +1,13 @@
+package com.csc530.familytree.controllers
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EditMemberActivity : AppCompatActivity()
+{
+	override fun onCreate(savedInstanceState: Bundle?)
+	{
+		super.onCreate(savedInstanceState)
+		setContentView(R.layout.activity_edit_member)
+	}
+}
