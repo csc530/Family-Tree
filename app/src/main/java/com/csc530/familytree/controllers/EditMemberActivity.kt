d44ba13ba5a3473a94f4224e913124331bddae37
csc530 <cscousin@lakeheadu.ca>
2022-03-08 16:20:18 -0500

    :lipstick: style(views): Activities
    Created basic template and content to the edit/add family member screen
    
    Signed-off-by: csc530 <cscousin@lakeheadu.ca>

--- app/src/main/java/com/csc530/familytree/controllers/EditMemberActivity.kt
+++ app/src/main/java/com/csc530/familytree/controllers/EditMemberActivity.kt
@@ -2,12 +2,15 @@ package com.csc530.familytree.controllers
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.csc530.familytree.databinding.ActivityEditMemberBinding
 
 class EditMemberActivity : AppCompatActivity()
 {
+	private lateinit var binding: ActivityEditMemberBinding
 	override fun onCreate(savedInstanceState: Bundle?)
 	{
 		super.onCreate(savedInstanceState)
-		setContentView(R.layout.activity_edit_member)
+		binding = ActivityEditMemberBinding.inflate(layoutInflater)
+		setContentView(binding.root)
 	}
 }
