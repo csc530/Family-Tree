86c01a2a8186c86f6de3ee05e99cfe84ed6f8695
Christofer <77406318+csc530@users.noreply.github.com>
2022-03-08 14:40:40 -0500

    :fire: revert(project): Family member
    Deleted the (attempted) use to use a recyclerview to display a family member and in turn create a family tree
    
    Signed-off-by: csc530 <cscousin@lakeheadu.ca>

--- app/src/main/java/com/csc530/familytree/controllers/TreeActivity.kt
+++ app/src/main/java/com/csc530/familytree/controllers/TreeActivity.kt
@@ -2,31 +2,26 @@ package com.csc530.familytree.controllers
 
 
 import android.os.Bundle
-import android.util.Log
-import android.widget.TextView
-import android.widget.Toast
-import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.children
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.auth.ktx.auth
-import com.google.firebase.firestore.FirebaseFirestore
-import com.google.firebase.ktx.Firebase
 import com.csc530.familytree.databinding.ActivityTreeBinding
-import com.csc530.familytree.views.MemberAdapter
-import com.csc530.familytree.views.MemberViewModel
 
-class TreeActivity : AppCompatActivity() {
+class TreeActivity : AppCompatActivity()
+{
 	private lateinit var binding: ActivityTreeBinding
 	
-	override fun onCreate(savedInstanceState: Bundle?) {
+	override fun onCreate(savedInstanceState: Bundle?)
+	{
 		super.onCreate(savedInstanceState)
 		binding = ActivityTreeBinding.inflate(layoutInflater)
 		setContentView(binding.root)
-		//connect RecyclerView with FirestoreDB via the ViewModel
-		val viewModel: MemberViewModel by viewModels()
-		viewModel.getProjects().observe(this) { members ->
-			binding.recyclerView.adapter = MemberAdapter(this, members)
-		}
+//		//connect RecyclerView with FirestoreDB via the ViewModel
+//		val viewModel: MemberViewModel by viewModels()
+//		viewModel.getProjects().observe(this) {
+//			binding.recyclerView.adapter = MemberAdapter(this, it)
+//		}
+//		binding.button2.setOnClickListener {
+//			val db = FirebaseFirestore.getInstance().collection("tests")
+//			db.add(Member("pop", "dsa", null, null, null, null))
+//		}
 	}
 }
