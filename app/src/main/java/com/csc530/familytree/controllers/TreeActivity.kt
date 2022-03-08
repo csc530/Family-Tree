9b52b15e019abcae58053adb5b316a7a08972cb4
csc530 <77406318+csc530@users.noreply.github.com>
2022-03-08 15:14:45 -0500

    :sparkles: feat(family-tree): Family member
    Can create a new family member view and add it to the screen
    
    Signed-off-by: csc530 <cscousin@lakeheadu.ca>

--- app/src/main/java/com/csc530/familytree/controllers/TreeActivity.kt
+++ app/src/main/java/com/csc530/familytree/controllers/TreeActivity.kt
@@ -1,9 +1,11 @@
 package com.csc530.familytree.controllers
 
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import com.csc530.familytree.databinding.ActivityTreeBinding
+import com.csc530.familytree.views.MemberView
 
 class TreeActivity : AppCompatActivity()
 {
@@ -14,14 +16,20 @@ class TreeActivity : AppCompatActivity()
 		super.onCreate(savedInstanceState)
 		binding = ActivityTreeBinding.inflate(layoutInflater)
 		setContentView(binding.root)
-//		//connect RecyclerView with FirestoreDB via the ViewModel
-//		val viewModel: MemberViewModel by viewModels()
-//		viewModel.getProjects().observe(this) {
-//			binding.recyclerView.adapter = MemberAdapter(this, it)
-//		}
-//		binding.button2.setOnClickListener {
-//			val db = FirebaseFirestore.getInstance().collection("tests")
-//			db.add(Member("pop", "dsa", null, null, null, null))
-//		}
+		binding.imgbtnAdd.setOnClickListener { btn ->
+			val member = MemberView(this)
+			member.firstName = "New name"
+			member.fontColour = Color.CYAN
+			binding.root.addView(member)
+		}
+		//		//connect RecyclerView with FirestoreDB via the ViewModel
+		//		val viewModel: MemberViewModel by viewModels()
+		//		viewModel.getProjects().observe(this) {
+		//			binding.recyclerView.adapter = MemberAdapter(this, it)
+		//		}
+		//		binding.button2.setOnClickListener {
+		//			val db = FirebaseFirestore.getInstance().collection("tests")
+		//			db.add(Member("pop", "dsa", null, null, null, null))
+		//		}
 	}
 }
--- app/src/main/java/com/csc530/familytree/views/MemberView.kt
+++ app/src/main/java/com/csc530/familytree/views/MemberView.kt
@@ -8,14 +8,16 @@ import android.view.View
 import android.view.ViewTreeObserver.OnGlobalLayoutListener
 import com.csc530.familytree.R
 
-class MemberView : View {
+class MemberView : View
+{
 	
 	private val DEFAULT_FONT_SIZE: Float = 25f
 	private val DEFAULT_FONT_COLOUR: Int = Color.BLACK
 	private val DEFAULT_PORTRAIT = R.drawable.user
 	
 	private var portrait: Bitmap = BitmapFactory.decodeResource(resources, R.drawable.user)
-		set(value) {
+		set(value)
+		{
 			field = value
 			postInvalidate()
 		}
@@ -23,8 +25,9 @@ class MemberView : View {
 	/**
 	 * The text to draw; member's first name
 	 */
-	var firstName: String? = "Talon"
-		set(value) {
+	var firstName: String = "Talon"
+		set(value)
+		{
 			field = value
 			textWidth = textPaint.measureText(field)
 			textHeight = textPaint.fontMetrics.bottom
@@ -45,7 +48,8 @@ class MemberView : View {
 	 */
 	var fontColour: Int = R.color.black
 		//TODO: change to colour on primary
-		set(value) {
+		set(value)
+		{
 			field = value
 			textPaint.color = field
 			postInvalidate()
@@ -55,7 +59,8 @@ class MemberView : View {
 	 * In the example view, this dimension is the font size.
 	 */
 	var fontSize: Float = 25f
-		set(value) {
+		set(value)
+		{
 			field = value
 			textPaint.textSize = field
 			postInvalidate()
@@ -63,54 +68,64 @@ class MemberView : View {
 	
 	
 	// * BELOW - necessary parent constructors form View class
-	constructor(context: Context) : super(context) {
+	constructor(context: Context) : super(context)
+	{
 		init(null, 0)
 	}
 	
-	constructor(context: Context, attrs: AttributeSet) : super(context, attrs) {
+	constructor(context: Context, attrs: AttributeSet) : super(context, attrs)
+	{
 		init(attrs, 0)
 	}
 	
-	constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle) {
+	constructor(context: Context, attrs: AttributeSet, defStyle: Int) : super(context, attrs, defStyle)
+	{
 		init(attrs, defStyle)
 	}
 	
-	private fun init(attributeSet: AttributeSet?, defStyle: Int) {
+	private fun init(attributeSet: AttributeSet?, defStyle: Int)
+	{
 		// Load attributes//TODO change view class name to MemberView same as declarableStyle name
 		val a = context.obtainStyledAttributes(
-			attributeSet, R.styleable.MemberView, defStyle, 0)
+				attributeSet, R.styleable.MemberView, defStyle, 0)
 		
 		firstName = a.getString(
-			R.styleable.MemberView_firstName)
+				R.styleable.MemberView_firstName).toString()
 		fontColour = a.getColor(
-			R.styleable.MemberView_fontColour,
-			DEFAULT_FONT_COLOUR)
+				R.styleable.MemberView_fontColour,
+				DEFAULT_FONT_COLOUR)
 		// Use getDimensionPixelSize or getDimensionPixelOffset when dealing with
 		// values that should fall on pixel boundaries.
 		fontSize = a.getDimension(
-			R.styleable.MemberView_fontSize,
-			DEFAULT_FONT_SIZE)
+				R.styleable.MemberView_fontSize,
+				DEFAULT_FONT_SIZE)
 		
-		if(a.hasValue(R.styleable.MemberView_portrait)) {
+		if(a.hasValue(R.styleable.MemberView_portrait))
+		{
 			val drawable = a.getResourceId(R.styleable.MemberView_portrait, DEFAULT_PORTRAIT)
 			portrait = BitmapFactory.decodeResource(resources, drawable)
 			/*? This is to set the portrait size to be within the view's bounds (height&width)
 			? Necessary because within the init it hasn't drawn (know) it's dimensions*/
-			viewTreeObserver.addOnGlobalLayoutListener(object : OnGlobalLayoutListener {
-				override fun onGlobalLayout() {
-					viewTreeObserver.removeOnGlobalLayoutListener(this)
-					portrait = resizeBitmap(portrait, width.toFloat(), height.toFloat())
-				}
-			})
+			viewTreeObserver.addOnGlobalLayoutListener(
+					// ? Resizes the image whenever the container's width or height changes to remain within it
+					object : OnGlobalLayoutListener
+					{
+						override fun onGlobalLayout()
+						{
+							if(width < portrait.width || height < portrait.height) return;
+							viewTreeObserver.removeOnGlobalLayoutListener(this)
+							portrait = resizeBitmap(portrait, width.toFloat(), height.toFloat())
+						}
+					})
 		}
-		
 		a.recycle()
 		
 		// Update TextPaint and text measurements from attributes
 		postInvalidate()
 	}
 	
-	private fun resizeBitmap(bitmap: Bitmap, width: Float, height: Float): Bitmap {
+	private fun resizeBitmap(bitmap: Bitmap, width: Float, height: Float): Bitmap
+	{
 		//? Thank you to Coderz Geek - https://youtu.be/BxBcs1ddEn8?t=350
 		val matrix = Matrix()
 		val src = RectF(0F, 0f, bitmap.width.toFloat(), bitmap.height.toFloat())
@@ -119,7 +134,8 @@ class MemberView : View {
 		return Bitmap.createBitmap(bitmap, 0, 0, bitmap.width, bitmap.height, matrix, true)
 	}
 	
-	override fun onDraw(canvas: Canvas?) {
+	override fun onDraw(canvas: Canvas?)
+	{
 		super.onDraw(canvas)
 		
 		//? set the width that content can take up
@@ -128,7 +144,8 @@ class MemberView : View {
 		
 		updateTextPaint()
 		portrait = resizeBitmap(portrait, contentWidth, contentHeight / 2f)
-		if(portrait.width.toFloat() != contentWidth) {
+		if(portrait.width.toFloat() != contentWidth)
+		{
 			val centerX: Float = (paddingLeft + contentWidth / 2f) - portrait.width / 2f
 			canvas?.drawBitmap(portrait, centerX, paddingTop.toFloat(), null)
 		}
@@ -136,17 +153,15 @@ class MemberView : View {
 			canvas?.drawBitmap(portrait, paddingLeft.toFloat(), paddingTop.toFloat(), null)
 		firstName?.let {
 			// Draw the text.
-			canvas?.drawText(it,
-			                 (paddingLeft + contentWidth/2) - textWidth/2f,
-				// ? place text below the image
-				             paddingTop + (contentHeight + portrait.height + textHeight) / 2,
-				             textPaint)
+			canvas?.drawText(it, (paddingLeft + contentWidth / 2) - textWidth / 2f,
+					// ? place text below the image
+					         paddingTop + (contentHeight + portrait.height + textHeight) / 2,
+					         textPaint)
 		}
-		
-		
 	}
 	
-	private fun updateTextPaint() {
+	private fun updateTextPaint()
+	{
 		textPaint.let {
 			it.textSize = fontSize
 			it.color = fontColour
