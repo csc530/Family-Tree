1abb1f07cfd270817921fed4b78b2669c14efbd6
csc530 <cscousin@lakeheadu.ca>
2022-03-23 15:55:13 -0400

    refactor(views): Activities
    Changed/Added activity for viewing and to edit a family member
    
    Signed-off-by: csc530 <cscousin@lakeheadu.ca>

--- app/src/main/java/com/csc530/familytree/controllers/MemberDetailsActivity.kt
+++ app/src/main/java/com/csc530/familytree/controllers/MemberDetailsActivity.kt
@@ -2,15 +2,15 @@ package com.csc530.familytree.controllers
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import com.csc530.familytree.databinding.ActivityEditDetailsBinding
+import com.csc530.familytree.databinding.ActivityMemberDetailsBinding
 
-class EditDetailsActivity : AppCompatActivity() {
+class MemberDetailsActivity : AppCompatActivity() {
 	//!!TODO make it like a fragment where you click the member and can swipe down the view
 	//with like a tab at the bottom to swipe it back up with persisting and changing info depending on the source member
-	private lateinit var binding: ActivityEditDetailsBinding
+	private lateinit var binding: ActivityMemberDetailsBinding
 	override fun onCreate(savedInstanceState: Bundle?) {
 		super.onCreate(savedInstanceState)
-		binding = ActivityEditDetailsBinding.inflate(layoutInflater)
+		binding = ActivityMemberDetailsBinding.inflate(layoutInflater)
 		setContentView(binding.root)
 	}
 }
